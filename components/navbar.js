a56bb81e95f0ab4e5e27078ee44ddc1832a7b92b
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2022-12-02 19:03:52 -0800

    fixed compiler errors

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,19 @@
 import React from "react";
-import "../Home.module.css";
+import "../styles/Home.module.css";
 
 function Navbar() {
-	return (
-        <div className="Navbar">
-            <div className="leftSide">
-                <div classname="links">
-                    <a href="/events">Events</a>
-                    <a href="/contact">Contact</a>
-                    <a href="/filter">Search</a>
-                </div>
-            </div>
-            <div className="rightSide"></div>
+  return (
+    <div className="Navbar">
+      <div className="leftSide">
+        <div classname="links">
+          <a href="/events">Events</a>
+          <a href="/contact">Contact</a>
+          <a href="/filter">Search</a>
         </div>
-    );
+      </div>
+      <div className="rightSide"></div>
+    </div>
+  );
 }
 
 export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import '../styles/globals.css'
-import Navbar from './Components/Navbar';
+import "../styles/globals.css";
+import Navbar from "../Components/Navbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
   // return <div className="MyApp"><Navbar /></div>
 }
 
-export default MyApp
+export default MyApp;
