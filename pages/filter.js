810506efbda5261ac3b84af39b1b629627946a4b
rjwarrick <80236919+rjwarrick@users.noreply.github.com>
2022-12-02 16:12:18 -0800

    Started on filter page

---
+++ pages/filter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Category = () => {
+    <>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <hi>Filter</hi>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <button className='btn'>San Francisco</button>
+                    <button className='btn'>East Bay</button>
+                    <button className='btn'>North Bay</button>
+                    <button className='btn'>South Bay</button>
+                </div>
+                <div classname="col">
+                    Right
+                </div>
+            </div>
+        </div>
+    </>
+}
