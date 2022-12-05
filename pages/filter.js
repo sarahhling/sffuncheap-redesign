4321dde00778e1cd6a1fbc126736c52bfeeb939d
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2022-12-05 01:43:34 -0800

    fix build errors

--- pages/filter.js
+++ pages/filter.js
@@ -1,62 +1,64 @@
 import React from 'react';
+export default function Filter() {
+  return (<div></div>);
 
-const Category = () => {
-    <>
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <hi>Filter</hi>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <button className='btn'>San Francisco</button>
-                    <button className='btn'>East Bay</button>
-                    <button className='btn'>North Bay</button>
-                    <button className='btn'>South Bay</button>
-                </div>
-                <div classname="col">
-                    <div className="cards">
-                        <div className="card">
-                            <div className="card-header">
-                                {/* insert image here */}
-                            </div>
-                            <div className="card-body">
-                                <h2 className='eventName'>Event 1</h2>
-                                <span className="date">Date:</span>
-                                <span className="time">Time:</span>
-                                <span className="Location">Date:</span>
-                                <span className="Cost">Cost: Free</span>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-header">
-                                {/* insert image here */}
-                            </div>
-                            <div className="card-body">
-                                <h2 className='eventName'>Event 2</h2>
-                                <span className="date">Date:</span>
-                                <span className="time">Time:</span>
-                                <span className="Location">Date:</span>
-                                <span className="Cost">Cost: $7</span>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-header">
-                                {/* insert image here */}
-                            </div>
-                            <div className="card-body">
-                                <h2 className='eventName'>Event 3</h2>
-                                <span className="date">Date:</span>
-                                <span className="time">Time:</span>
-                                <span className="Location">Date:</span>
-                                <span className="Cost">Cost: Free</span>
-                            </div>
-                        </div>
-                    </div>
-                    Right
-                </div>
-            </div>
-        </div>
-    </>
-}
+// const Category = () => {
+//     <>
+//         <div className="container">
+//             <div className="row">
+//                 <div className="col">
+//                     <hi>Filter</hi>
+//                 </div>
+//             </div>
+//             <div className="row">
+//                 <div className="col">
+//                     <button className='btn'>San Francisco</button>
+//                     <button className='btn'>East Bay</button>
+//                     <button className='btn'>North Bay</button>
+//                     <button className='btn'>South Bay</button>
+//                 </div>
+//                 <div classname="col">
+//                     <div className="cards">
+//                         <div className="card">
+//                             <div className="card-header">
+//                                 {/* insert image here */}
+//                             </div>
+//                             <div className="card-body">
+//                                 <h2 className='eventName'>Event 1</h2>
+//                                 <span className="date">Date:</span>
+//                                 <span className="time">Time:</span>
+//                                 <span className="Location">Date:</span>
+//                                 <span className="Cost">Cost: Free</span>
+//                             </div>
+//                         </div>
+//                         <div className="card">
+//                             <div className="card-header">
+//                                 {/* insert image here */}
+//                             </div>
+//                             <div className="card-body">
+//                                 <h2 className='eventName'>Event 2</h2>
+//                                 <span className="date">Date:</span>
+//                                 <span className="time">Time:</span>
+//                                 <span className="Location">Date:</span>
+//                                 <span className="Cost">Cost: $7</span>
+//                             </div>
+//                         </div>
+//                         <div className="card">
+//                             <div className="card-header">
+//                                 {/* insert image here */}
+//                             </div>
+//                             <div className="card-body">
+//                                 <h2 className='eventName'>Event 3</h2>
+//                                 <span className="date">Date:</span>
+//                                 <span className="time">Time:</span>
+//                                 <span className="Location">Date:</span>
+//                                 <span className="Cost">Cost: Free</span>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     Right
+//                 </div>
+//             </div>
+//         </div>
+//     </>
+// }
