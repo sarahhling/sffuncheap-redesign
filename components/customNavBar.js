654629610f1a4437b288a99e6983e4b4dda21969
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2022-12-07 15:07:23 -0800

    edit photo size

--- components/customNavBar.js
+++ components/customNavBar.js
@@ -47,7 +47,7 @@ export default function CustomNavBar() {
             <i class="fas fa-search"></i>
           </button>
         </div> */}
-        <div class="input-group ps-2 pe-3">
+        <div className="input-group ps-2 pe-3">
           <input
             type="text"
             className={`form-control input-sm ${styles.searchBar}`}
@@ -62,7 +62,7 @@ export default function CustomNavBar() {
               width="15"
               height="15"
               fill="currentColor"
-              class="bi bi-search"
+              className="bi bi-search"
               viewBox="0 0 16 16"
             >
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
